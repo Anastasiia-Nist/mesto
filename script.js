9c0f02191027bc71eb9b981b250f373f5026690d
Anastasia Nistratova <a.nistratova14@gmail.com>
2023-02-04 08:29:30 +0300

    fix: поправить код после первого ревью

--- script.js
+++ script.js
@@ -1,33 +1,31 @@
-//открытие и закрытие попапа
 let popup = document.querySelector('.popup');
-let profile__button_edit = document.querySelector('.profile__button-edit');
-let popup__button_close = document.querySelector('.popup__button-close');
+let profileButtonEdit = document.querySelector('.profile__button-edit');
+let popupButtonClose = document.querySelector('.popup__button-close');
+let profileName =  document.querySelector('.profile__name');
+let profileCareer = document.querySelector('.profile__career');
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__input_details_name');
+let careerInput = document.querySelector('.popup__input_details_career');
 
-function showPopup() {
+//открытие попапа
+function openedPopup() {
+    popup.classList.toggle('popup_opened');
+    nameInput.value = profileName.textContent;
+    careerInput.value = profileCareer.textContent;
+}
+//закрытие попапа
+function closedPopup() {
     popup.classList.toggle('popup_opened');
 }
 
-profile__button_edit.addEventListener('click', showPopup);
-popup__button_close.addEventListener('click', showPopup);
-
-//отправка формы и редактирование profile
-let formElement = document.querySelector('.popup__form');
-let popup__button_save = document.querySelector('.popup__button-save');
-
-let nameInput = document.querySelector('.popup__input-name');
-let jobInput = document.querySelector('.popup__input-career');
-
-
+//отправка формы
 function handleFormSubmit (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-
-    let profileName =  document.querySelector('.profile__name');
-    let profileCareer = document.querySelector('.profile__career');
-
+    evt.preventDefault();
     profileName.textContent = nameInput.value;
-    profileCareer.textContent = jobInput.value;
-   
+    profileCareer.textContent = careerInput.value;
+    closedPopup();
 }
 
+profileButtonEdit.addEventListener('click', openedPopup);
+popupButtonClose.addEventListener('click', closedPopup);
 formElement.addEventListener('submit', handleFormSubmit);
-popup__button_save.addEventListener('click', showPopup);
